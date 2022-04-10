a1ae14f0fd808a49ef06511b3e54e6aa6cad8239
Muhammad Ismail <yahyaismail@gmail.com>
2022-04-10 11:20:41 -0400

    Application.py: Add askNumSensors method, numSensors attribute and accessor/mutator methods

--- Application.py
+++ Application.py
@@ -2,9 +2,31 @@ class Application:
 
     def __init__(self):
         self._listSensors = []
+        self._numSensors = 0
 
     def getListSensors(self):
         return self._listSensors
 
+    def getNumSensors(self):
+        return self._numSensors
+
     def setListSensors(self, newListSensors):
         self._listSensors = newListSensors
+
+    def setNumSensors(self, newNumSensors):
+        self._numSensors = newNumSensors
+
+    def askNumSensors(self):
+        validInput = False
+        while validInput is not True:
+            try:
+                numSensors = int(input("Enter the number of sensors: "))
+            except:
+                print("This is an invalid entry for the number of sensors!")
+            else:
+                self.setNumSensors = numSensors
+                validInput = True
+
+
+App = Application()
+App.askNumSensors()
